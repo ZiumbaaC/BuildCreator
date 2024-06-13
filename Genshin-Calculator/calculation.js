185d2230d84534d9da1d726282463c03fc231de3
ZiumbaaC <ziumbaa.yt@gmail.com>
2024-06-13 20:17:18 +0200

    update

---
+++ Genshin-Calculator/calculation.js
@@ -0,0 +1,7 @@
+const atk = document.getElementById("atkInput").innerHTML;
+const def = document.getElementById("defInput").innerHTML;
+const hp = document.getElementById("hpInput").innerHTML;
+const em = document.getElementById("hpInput").innerHTML;
+const cd = document.getElementById("cdInput").innerHTML;
+const db = document.getElementById("dbInput").innerHTML;
+
