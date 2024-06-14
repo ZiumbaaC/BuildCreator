0275da156f9d9b507d837c65d61168ae35e5a30c
ZiumbaaC <ziumbaa.yt@gmail.com>
2024-06-14 13:40:37 +0200

    update

--- Genshin-Calculator/calculation.js
+++ Genshin-Calculator/calculation.js
@@ -5,6 +5,7 @@ const em = document.getElementById("emInput").innerHTML;
 const cd = document.getElementById("cdInput").innerHTML;
 const cr = document.getElementById("crInput").innerHTML;
 const db = document.getElementById("dbInput").innerHTML;
+const as = document.getElementById("asInput").innerHTML;
 const element = document.getElementById("elementInput").innerHTML;
 
 document.getElementById("emInput").hidden = true;
@@ -12,15 +13,20 @@ document.getElementById("emInput").hidden = true;
 document.getElementById("elementInput").addEventListener("change", () => {
     if(element != "physical") {
         document.getElementById("emInput").hidden = false;
+        document.getElementById("emLabel").hidden = false;
+    }
+    else {
+        document.getElementById("emInput").hidden = true;
+        document.getElementById("emLabel").hidden = true;
     }
 });
 
 
-let baseDmg = dmgScaling;
+const baseDmg = as;
 
 
-let damage = (baseDmg * baseDmgMulti + additiveBaseDmg) * dmgBonusMulti * defMulti * resMulti * amplifyingMulti;
+const damage = (baseDmg * baseDmgMulti + additiveBaseDmg) * dmgBonusMulti * defMulti * resMulti * amplifyingMulti;
 
-let critDamage = damage * (1 + cd / 100);
+const critDamage = damage * (1 + cd / 100);
 
-let averageDamage = critDamage * (cr / 100) + damage * (1 - cr / 100);
+const averageDamage = critDamage * (cr / 100) + damage * (1 - cr / 100);
