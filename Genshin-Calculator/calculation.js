3ea41ba0cde8616010b51938d9cb835d5ed22771
ZiumbaaC <ziumbaa.yt@gmail.com>
2024-06-14 13:12:17 +0200

    update

--- Genshin-Calculator/calculation.js
+++ Genshin-Calculator/calculation.js
@@ -1,7 +1,26 @@
 const atk = document.getElementById("atkInput").innerHTML;
 const def = document.getElementById("defInput").innerHTML;
 const hp = document.getElementById("hpInput").innerHTML;
-const em = document.getElementById("hpInput").innerHTML;
+const em = document.getElementById("emInput").innerHTML;
 const cd = document.getElementById("cdInput").innerHTML;
+const cr = document.getElementById("crInput").innerHTML;
 const db = document.getElementById("dbInput").innerHTML;
+const element = document.getElementById("elementInput").innerHTML;
 
+document.getElementById("emInput").hidden = true;
+
+document.getElementById("elementInput").addEventListener("change", () => {
+    if(element != "physical") {
+        document.getElementById("emInput").hidden = false;
+    }
+});
+
+
+let baseDmg = dmgScaling;
+
+
+let damage = (baseDmg * baseDmgMulti + additiveBaseDmg) * dmgBonusMulti * defMulti * resMulti * amplifyingMulti;
+
+let critDamage = damage * (1 + cd / 100);
+
+let averageDamage = critDamage * (cr / 100) + damage * (1 - cr / 100);
